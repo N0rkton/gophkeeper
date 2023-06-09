7b020b30994b2510758544c4438805a63f678ace
N0rkton <shvm20031807@gmail.com>
2023-06-09 14:41:35 +0300

    review

--- server/grpcfuncs/grpcfuctions_test.go
+++ server/grpcfuncs/grpcfuctions_test.go
@@ -2,6 +2,11 @@ package grpcfuncs_test
 
 import (
 	"context"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+	"gophkeeper/server/grpcfuncs"
+	"log"
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,12 +17,25 @@ import (
 
 func TestAuth(t *testing.T) {
 	// Start the gRPC server in a separate goroutine
+
 	go func() {
+		grpcfuncs.Init()
+		listen, err := net.Listen("tcp", ":3200")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		s := grpc.NewServer()
+		pb.RegisterGophkeeperServer(s, &grpcfuncs.GophKeeperServer{})
+
+		if err := s.Serve(listen); err != nil {
+			log.Fatal(err)
+		}
 		// Start your gRPC server implementation
 	}()
 
 	// Connect the client to the server
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.Dial(":3200", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	assert.NoError(t, err)
 	defer conn.Close()
 
@@ -26,13 +44,16 @@ func TestAuth(t *testing.T) {
 
 	// Perform the gRPC request
 	request := &pb.AuthLoginRequest{
-		// Set the request fields accordingly
+		Login:    "test",
+		Password: "password",
 	}
-	response, err := client.Auth(context.Background(), request)
+	var header metadata.MD
+	response, err := client.Auth(context.Background(), request, grpc.Header(&header))
 
 	// Assert the expected response
 	assert.NoError(t, err)
 	assert.NotNil(t, response)
+	assert.NotNil(t, header)
 	// Add more assertions as needed
 }
 
