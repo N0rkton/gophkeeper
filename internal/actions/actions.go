a3472c102d6ed231c9969d541a8c383a85dfa73a
N0rkton <shvm20031807@gmail.com>
2023-06-18 22:06:35 +0300

    cli funcs

--- internal/actions/actions.go
+++ internal/actions/actions.go
@@ -8,6 +8,26 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func auth(store storage.Storage) func(ctx *cli.Context) error {
+	return func(ctx *cli.Context) error {
+		n := ctx.NArg()
+		if n == 0 {
+			return fmt.Errorf("no argument provided for auth")
+		}
+		if n != 2 {
+			return fmt.Errorf("not enough arguments provided for auth")
+		}
+		login := ctx.Args().Get(0)
+		password := ctx.Args().Get(1)
+		err := store.Auth(login, password)
+		if err != nil {
+			return fmt.Errorf("error happend: %w", err)
+		}
+		fmt.Println("successful auth")
+		return nil
+	}
+}
+
 // Auth - used to authenticate new users;
 func Auth(store storage.Storage) *cli.Command {
 	return &cli.Command{
@@ -15,23 +35,37 @@ func Auth(store storage.Storage) *cli.Command {
 		Usage: "used to authenticate new users; you need to enter login and password; example: go run main.go auth login password",
 
 		Aliases: []string{"a", "auth"},
-		Action: func(ctx *cli.Context) error {
-			n := ctx.NArg()
-			if n == 0 {
-				return fmt.Errorf("no argument provided for auth")
-			}
-			if n != 2 {
-				return fmt.Errorf("not enough arguments provided for auth")
-			}
-			login := ctx.Args().Get(0)
-			password := ctx.Args().Get(1)
-			err := store.Auth(login, password)
-			if err != nil {
-				return fmt.Errorf("error happend: %w", err)
-			}
-			fmt.Println("successful auth")
-			return nil
-		},
+		Action:  auth(store),
+	}
+}
+
+func addData(store storage.Storage) func(ctx *cli.Context) error {
+	return func(ctx *cli.Context) error {
+		n := ctx.NArg()
+		if n == 0 {
+			return fmt.Errorf("no argument provided for auth")
+		}
+		if n < 4 {
+			return fmt.Errorf("not enough arguments provided for auth")
+		}
+		login := ctx.Args().Get(0)
+		password := ctx.Args().Get(1)
+
+		id, err := store.Login(login, password)
+		if err != nil {
+			return fmt.Errorf("error login happend: %w", err)
+		}
+		var data datamodels.Data
+		data.DataID = ctx.Args().Get(2)
+		data.Data = ctx.Args().Get(3)
+		data.Metadata = ctx.Args().Get(4)
+		data.UserID = id
+		err = store.AddData(data)
+		if err != nil {
+			return fmt.Errorf("error add happend: %w", err)
+		}
+		fmt.Println("data added successfully")
+		return nil
 	}
 }
 
@@ -41,33 +75,31 @@ func AddData(store storage.Storage) *cli.Command {
 		Name:    "addData",
 		Usage:   "used to add new data to keep it; you need to enter login and password, then data name, data and meta information if needed; example: go run main.go add login password dataID data metaData",
 		Aliases: []string{"add"},
-		Action: func(ctx *cli.Context) error {
-			n := ctx.NArg()
-			if n == 0 {
-				return fmt.Errorf("no argument provided for auth")
-			}
-			if n < 4 {
-				return fmt.Errorf("not enough arguments provided for auth")
-			}
-			login := ctx.Args().Get(0)
-			password := ctx.Args().Get(1)
-
-			id, err := store.Login(login, password)
-			if err != nil {
-				return fmt.Errorf("error login happend: %w", err)
-			}
-			var data datamodels.Data
-			data.DataID = ctx.Args().Get(2)
-			data.Data = ctx.Args().Get(3)
-			data.Metadata = ctx.Args().Get(4)
-			data.UserID = id
-			err = store.AddData(data)
-			if err != nil {
-				return fmt.Errorf("error add happend: %w", err)
-			}
-			fmt.Println("data added successfully")
-			return nil
-		},
+		Action:  addData(store),
+	}
+}
+func getData(store storage.Storage) func(ctx *cli.Context) error {
+	return func(ctx *cli.Context) error {
+		n := ctx.NArg()
+		if n == 0 {
+			return fmt.Errorf("no argument provided for auth")
+		}
+		if n != 3 {
+			return fmt.Errorf("wrong amount of arguments")
+		}
+		login := ctx.Args().Get(0)
+		password := ctx.Args().Get(1)
+		id, err := store.Login(login, password)
+		if err != nil {
+			return fmt.Errorf("error login happend: %w", err)
+		}
+		dataId := ctx.Args().Get(2)
+		data, err := store.GetData(dataId, id)
+		if err != nil {
+			return fmt.Errorf("error get happend: %w", err)
+		}
+		fmt.Println("DataID: " + data.DataID + " Data: " + data.Data + " Meta Info: " + data.Metadata)
+		return nil
 	}
 }
 
@@ -77,28 +109,31 @@ func GetData(store storage.Storage) *cli.Command {
 		Name:    "get data",
 		Usage:   "used to get data ; you need to enter login and password, then data name; example: go run main.go get login password dataId",
 		Aliases: []string{"get", "g"},
-		Action: func(ctx *cli.Context) error {
-			n := ctx.NArg()
-			if n == 0 {
-				return fmt.Errorf("no argument provided for auth")
-			}
-			if n != 3 {
-				return fmt.Errorf("wrong amount of arguments")
-			}
-			login := ctx.Args().Get(0)
-			password := ctx.Args().Get(1)
-			id, err := store.Login(login, password)
-			if err != nil {
-				return fmt.Errorf("error login happend: %w", err)
-			}
-			dataId := ctx.Args().Get(2)
-			data, err := store.GetData(dataId, id)
-			if err != nil {
-				return fmt.Errorf("error get happend: %w", err)
-			}
-			fmt.Println("DataID: " + data.DataID + " Data: " + data.Data + " Meta Info: " + data.Metadata)
-			return nil
-		},
+		Action:  getData(store),
+	}
+}
+func delData(store storage.Storage) func(ctx *cli.Context) error {
+	return func(ctx *cli.Context) error {
+		n := ctx.NArg()
+		if n == 0 {
+			return fmt.Errorf("no argument provided for auth")
+		}
+		if n != 3 {
+			return fmt.Errorf("wrong amount of arguments")
+		}
+		login := ctx.Args().Get(0)
+		password := ctx.Args().Get(1)
+		id, err := store.Login(login, password)
+		if err != nil {
+			return fmt.Errorf("error login happend: %w", err)
+		}
+		dataId := ctx.Args().Get(2)
+		err = store.DelData(dataId, id)
+		if err != nil {
+			return fmt.Errorf("error deletr happend: %w", err)
+		}
+		fmt.Println("data deleted successfully")
+		return nil
 	}
 }
 
@@ -108,28 +143,38 @@ func DelData(store storage.Storage) *cli.Command {
 		Name:    "Delete data",
 		Usage:   "used to delete data; you need to enter login and password, then data name; example: go run main.go del login password dataId",
 		Aliases: []string{"del", "d"},
-		Action: func(ctx *cli.Context) error {
-			n := ctx.NArg()
-			if n == 0 {
-				return fmt.Errorf("no argument provided for auth")
-			}
-			if n != 3 {
-				return fmt.Errorf("wrong amount of arguments")
-			}
-			login := ctx.Args().Get(0)
-			password := ctx.Args().Get(1)
-			id, err := store.Login(login, password)
-			if err != nil {
-				return fmt.Errorf("error login happend: %w", err)
-			}
-			dataId := ctx.Args().Get(2)
-			err = store.DelData(dataId, id)
-			if err != nil {
-				return fmt.Errorf("error deletr happend: %w", err)
-			}
-			fmt.Println("data deleted successfully")
-			return nil
-		},
+		Action:  delData(store),
+	}
+}
+
+func sync(store storage.Storage) func(ctx *cli.Context) error {
+	return func(ctx *cli.Context) error {
+		n := ctx.NArg()
+		if n == 0 {
+			return fmt.Errorf("no argument provided for auth")
+		}
+		if n != 2 {
+			return fmt.Errorf("wrong amount of arguments")
+		}
+		login := ctx.Args().Get(0)
+		password := ctx.Args().Get(1)
+		id, err := store.Login(login, password)
+		if err != nil {
+			return fmt.Errorf("error login happend: %w", err)
+		}
+		err = store.ClientSync(id, nil)
+		if err != nil {
+			return fmt.Errorf("error client sync happend: %w", err)
+		}
+		data, err := store.Sync(id)
+		if err != nil {
+			return fmt.Errorf("error sync happend: %w", err)
+		}
+		for _, v := range data {
+			fmt.Println("DataID: " + v.DataID + " Data: " + v.Data + " Meta Info: " + v.Metadata)
+		}
+
+		return nil
 	}
 }
 
@@ -139,34 +184,7 @@ func Sync(store storage.Storage) *cli.Command {
 		Name:    "synchronization",
 		Usage:   "used synchronize server and client; you need to enter login and password; example: go run main.go sync login password",
 		Aliases: []string{"sync", "s"},
-		Action: func(ctx *cli.Context) error {
-			n := ctx.NArg()
-			if n == 0 {
-				return fmt.Errorf("no argument provided for auth")
-			}
-			if n != 2 {
-				return fmt.Errorf("wrong amount of arguments")
-			}
-			login := ctx.Args().Get(0)
-			password := ctx.Args().Get(1)
-			id, err := store.Login(login, password)
-			if err != nil {
-				return fmt.Errorf("error login happend: %w", err)
-			}
-			err = store.ClientSync(id, nil)
-			if err != nil {
-				return fmt.Errorf("error client sync happend: %w", err)
-			}
-			data, err := store.Sync(id)
-			if err != nil {
-				return fmt.Errorf("error sync happend: %w", err)
-			}
-			for _, v := range data {
-				fmt.Println("DataID: " + v.DataID + " Data: " + v.Data + " Meta Info: " + v.Metadata)
-			}
-
-			return nil
-		},
+		Action:  sync(store),
 	}
 }
 
